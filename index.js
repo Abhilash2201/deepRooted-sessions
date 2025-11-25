3fdb07556fba68189f8e36386a763d73c28921b0
Abhilash2201 <abhilashl9845@gmail.com>
2025-11-25 08:52:42 +0530

    day 1

---
+++ index.js
@@ -0,0 +1,68 @@
+// console.log("i am a external file");
+
+// tokens
+
+// smallest unit in anu programming lang
+
+// keyword
+// operator +-*/=%
+// separtor ;,
+// symbols {}()[]
+// literals/identifier
+
+// keywords
+// Keywords in C are reserved words with special meanings, part of the syntax, and cannot be used as identifier
+
+// variables
+// named memory location used to store the data
+
+// data types
+
+// pimitive
+
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.undefined
+// 5.null
+// 6.BigInt
+// 7.Symbol
+
+// non-primitive
+
+// function
+// array
+// Object
+
+// console.log(5 + 5);
+// console.log(20 + 20);
+// console.log("the sum of 5 + 5 = 10");
+// console.log("the sum of 20 + 20 = 10");
+
+// dynamically typed lang
+
+// declare the variable
+
+// var , let , const
+
+// keyword identifier operator = data type separtor
+
+// var num1 = 10;
+// var num2 = 20;
+
+// console.log("the sum of " + num1 + "+" + num2 + "=" + (num1 + num2));
+// console.log("the difference of " + num1 + "-" + num2 + "=" + (num1 - num2));
+// console.log("the difference of " + num1 + "*" + num2 + "=" + num1 * num2);
+// console.log("the difference of " + num1 + "/" + num2 + "=" + num1 / num2);
+
+// var a = 10;
+// let b = 11;
+// const c = 12;
+
+// variable decalration
+var a;
+console.log(a);
+a = 10; //initialization
+console.log(a);
+a = 12; // reinitialization
+console.log(a);
